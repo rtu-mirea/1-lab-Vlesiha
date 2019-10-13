504caccefb806ff6d57ff0e0737be98e1cfa8797
Vlesiha <mascha.vla@yandex.ru>
2019-10-13 21:48:00 +0300

    Create string3.java

---
+++ Lab2/src/com/company/string3.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class string3 {
+    private String sentence3;
+    private String mainStr3 = new String();
+
+    public string3() {
+        sentence3 = "Цифры 11112345678, 1234634, 2524623523542, 3452523";
+        mainStr3 = new String(sentence3);
+    }
+    void strangeNumeric()
+    {
+        Pattern pattern = Pattern.compile("\\s*\\d*\\s*"); //поиск цифр
+        String[] words = mainStr3.split("[,\\s]"); //обрезаем строку
+        boolean found;
+        String newstr = "";
+        for (int i = 0; i<words.length;i++)
+        {
+            Matcher matcher = pattern.matcher(words[i]);
+            found=matcher.matches();
+            if (found)
+            {
+                StringBuilder strBild = new StringBuilder(words[i]);
+                for (int j = strBild.length(); j>0;j--)
+                {
+                    if ((strBild.length()-j)%4==0) // проверяем разряд
+                    {
+                        strBild.insert(j,".");
+                    }
+                }
+                words[i]=strBild.toString();
+            }
+            newstr+=words[i]+ " ";
+        }
+        System.out.println(newstr);
+    }
+
+}

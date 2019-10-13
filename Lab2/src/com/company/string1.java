77713197c1e07baa8684a0a3553b322fd4c1e970
Vlesiha <mascha.vla@yandex.ru>
2019-10-13 21:47:56 +0300

    Create string1.java

---
+++ Lab2/src/com/company/string1.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.regex.*;
+
+public class string1 {
+    private String sentence;
+    private String mainStr = new String();
+
+    public string1() {
+        sentence = "Мальчик в своем путешествии сделал большую ошибку. Он доверился незнакомому разбойнику. Разбойник отобрал все его ценности. она никогда не прощает. Цифры для примера 2, 5, 1, 7, 14, 15, 21";
+        mainStr = new String(sentence);
+    }
+    void vowel()
+    {
+        String[] copystr = mainStr.split(" ");
+        String substr = "";
+        for(String word : copystr) //проверяем все слова на начло и конец с гласной буквы
+        {
+            if ((word.startsWith("а") || word.startsWith("о") || word.startsWith("э") || word.startsWith("у") || word.startsWith("я") || word.startsWith("е") || word.startsWith("ю") || word.startsWith("и"))&&((word.endsWith("а") || word.endsWith("о") || word.endsWith("э") || word.endsWith("у") || word.endsWith("я") || word.endsWith("е") || word.endsWith("ю") || word.endsWith("и"))))
+            {
+                substr = substr + word + " ";
+            }
+        }
+        System.out.println(substr);
+    }
+    void wordlen(String in)
+    {
+        int count= 0;
+        String[] copystr = mainStr.split(" ");
+        String substr = "";
+        for(String word : copystr) //сравниваем длину слова с длинной вводимого слова
+        {
+            if (word.length() > in.length())
+            {
+                count++;
+            }
+        }
+        System.out.println("Количество слов по длинне больше заданного: "+count);
+    }
+    void prefix()
+    {
+        String[] copystr = mainStr.split(" ");
+        String substr = "";
+        for(String word : copystr) //проверяем текст на слова с пирставками на пре и при
+        {
+            if (word.startsWith("пре") || word.startsWith("при") || word.startsWith("При")||word.startsWith("Пре"))
+            {
+                substr = substr + word + " ";
+                substr.toLowerCase();
+            }
+        }
+        System.out.println(substr);
+    }
+    void numbers(int num1, int num2)
+    {
+        int count=0;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Pattern pat=Pattern.compile("[-]?[0-9]+(.[0-9]+)?");
+        Matcher matcher=pat.matcher(mainStr); //выделяем из текста все числа
+        while (matcher.find())
+        {
+            arrayList.add(Integer.parseInt(matcher.group())); //и добавляем в массив
+        }
+        for(int s : arrayList) //проверяем на принадлежность диапазону
+        {
+            if (s>num1 && s<num2)
+            {
+                count++;
+            }
+        }
+        System.out.println("Кол-во чисел, принадлежащих диапазону: " + count);
+    }
+}

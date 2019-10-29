32fa622f3b8b2e17815028d5fd7240f562849e71
Vlesiha <mascha.vla@yandex.ru>
2019-10-29 14:34:09 +0300

    Create admin.java

---
+++ lab3/src/com/company/admin.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class admin extends User{
+
+    public admin(String Name, String Login, String Password) {
+        super(Name, Login, Password);
+        option = 2;
+    }
+}

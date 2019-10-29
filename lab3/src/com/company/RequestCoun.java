421cb261fd4a86de39104e0233a54d2b6cf5e005
Vlesiha <mascha.vla@yandex.ru>
2019-10-29 14:34:17 +0300

    Create RequestCoun.java

---
+++ lab3/src/com/company/RequestCoun.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class RequestCoun {
+    private String countr;
+
+    public RequestCoun(){}
+
+    public void SetCoun(String country) {
+        countr = country;
+    }
+    public String getCountry()
+    {return countr;}
+
+}
